39781aed181a97a0519b8b56201538514adc71bc
BISHWAJIT SAHU <112744474+Bishwajits720@users.noreply.github.com>
2024-01-05 16:07:52 +0530

    @new 1

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App hiiiiiiiii hello/>
+    <App hii/>
     </BrowserRouter>
   </React.StrictMode>
 );
