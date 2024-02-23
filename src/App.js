fa738caa960a670764b0e93091ab4c7906cea553
HARSHITA77105 <21mca22@imit.ac.in>
2024-02-23 18:16:06 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import GridPractice from "./GridPractice";
-import Memories from './UI/Memories';
-import Sidebar from './UI/Sidebar';
+import Memories from "./UI/Memories";
+import Sidebar from "./UI/Sidebar";
 
-import Routing from './routes/Routing'
+import Routing from "./routes/Routing";
 
 function App() {
   return (
     <div className="App">
       {/* <GridPractice/> */}
-      <Memories/>
-      <Sidebar/>
+      <Memories />
+      <Sidebar />
 
-
-      
+      <h1>hello</h1>
 
       {/* <Routing/> */}
-
     </div>
-
-  
-    
   );
 }
 
